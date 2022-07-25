0ec2006bcd3624361da0c703feb2148459c069b7
nagubabusf <109980009+nagubabusf@users.noreply.github.com>
2022-07-25 22:27:47 +0530

    Update streamlit_app.py
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
-streamlit.title('Breakfast Menu')
+streamlit.header('Breakfast Menu')
+streamlit.text('Idly')
+streamlit.text('Dosa')
+streamlit.text('Poori')
